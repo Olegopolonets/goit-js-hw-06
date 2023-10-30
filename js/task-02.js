e9d855c2ec9e44456a47d169a8686292c66a8a2c
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-10-30 18:16:36 +0200

    completed 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+[...ingredients].forEach((item) => {
+  const listItem = document.createElement("li");
+  listItem.classList.add("item");
+  listItem.textContent = item;
+  ingredientsList.append(listItem);
+});
+
+console.log(ingredientsList);
