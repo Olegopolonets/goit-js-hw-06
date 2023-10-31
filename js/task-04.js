8591971c3d822e35dedef443d48411d3c8dbf27d
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-10-31 03:12:29 +0200

    add button: link-video in task-04

--- js/task-04.js
+++ js/task-04.js
@@ -7,13 +7,22 @@ const decrementBtn = document.querySelector(
 const incrementBtn = document.querySelector(
   '#counter button[data-action="increment"]'
 );
+const linkVideo = document.querySelector(".link-video");
+
+function isCorectNumber() {
+  return currentValue == 19
+    ? linkVideo.classList.remove("hidden")
+    : linkVideo.classList.add("hidden");
+}
 
 decrementBtn.addEventListener("click", () => {
   currentValue -= 1;
   endValue.textContent = `${currentValue}`;
+  isCorectNumber();
 });
 
 incrementBtn.addEventListener("click", () => {
   currentValue += 1;
   endValue.textContent = `${currentValue}`;
+  isCorectNumber();
 });
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,9 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// Яке число відобразить консоль?
+let two = 2;
+let five = 5;
+console.log(two * five + 9);
