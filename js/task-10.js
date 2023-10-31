dc87a30204a781309e3ed32de4c26e6956b73f52
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-10-31 13:39:16 +0200

    completed 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,38 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// Яке число відобразить консоль?
-let two = 2;
-let five = 5;
-console.log(two * five + 9);
+const controls = document.getElementById("controls");
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const boxes = document.getElementById("boxes");
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const width = i === 0 ? 30 : i * 10 + 30;
+    const height = i === 0 ? 30 : i * 10 + 30;
+    const color = getRandomHexColor();
+
+    const box = document.createElement("div");
+    box.style.width = width + "px";
+    box.style.height = height + "px";
+    box.style.backgroundColor = color;
+
+    boxes.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = Number(controls.querySelector("input").value);
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", () => {
+  destroyBoxes();
+});
