192bbb698abf73362e23ee2649db4f74f2f7b7b2
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-10-31 02:28:01 +0200

    completed 9 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
+const button = document.querySelector('[type="submit"]');
 
+function getValueOfEmail() {
+  const inputEmail = document.querySelector('[type="email"]');
+  if (inputEmail.value !== "") {
+    return inputEmail.value;
+  }
+  alert("Please fill all fields!");
+}
+function getValueOfPass() {
+  const inputPass = document.querySelector('[type="password"]');
+  if (inputPass.value !== "") {
+    return inputPass.value;
+  }
+  alert("Please fill all fields!");
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const emailCase = getValueOfEmail();
+  const passCase = getValueOfPass();
+  console.log({ emailCase, passCase });
+  form.reset();
+});
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,31 @@
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+const changeColor = document.querySelector(".change-color");
+
+changeColor.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+/* 
+Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+
+Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+*/
